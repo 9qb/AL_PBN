2ab925a8a4223eae53802e49d9fbb00d3efa79de
bli31 <bli31@stuy.edu>
2021-12-10 12:20:58 -0500

    init

---
+++ ALTester.java
@@ -0,0 +1,15 @@
+public class ALTester{
+  import ArrayList;
+  public ArrayList randle;
+  public boolean sorted;
+
+  public ALTester(){
+    randle = new ArrayList(23);
+  }
+
+  public void checkSort(){
+  //   for (int i = 0; i < randle.length; i++){
+  //     if (randle[i] < randle[])
+  //   }
+  }
+}
