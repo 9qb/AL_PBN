cea109ecbbb17cc0de597a3df742d4da4009ee2b
Brian Li <bli31@stuy.edu>
2021-12-11 11:04:13 -0500

    updated

--- ALTester.java
+++ ALTester.java
@@ -2,15 +2,31 @@ import java.util.ArrayList;
 
 public class ALTester{
   public ArrayList<Integer> randle;
-  public boolean sorted;
+  public int unsortedCount;
 
   public ALTester(){
     randle = new ArrayList<Integer>(23);
+    for (int counter = 0; counter < 23; counter++){
+      randle.add((int)(Math.random() * 100));
+    }
   }
 
-  public void checkSort(){
-    for (int i = 0; i < randle.length; i++){
-      if (randle[i].compareTo(randle[i+1]) > )
+  public boolean checkSort(){
+    for (int i = 0; i + 1 < randle.size(); i++){
+      if (randle.get(i) > randle.get(i+1)){
+        unsortedCount++;
+      }
     }
+    if (unsortedCount > 0){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+
+  public static void main(String[] args){
+    ALTester test = new ALTester();
+    System.out.println(test.checkSort());
   }
 }
