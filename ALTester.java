eb3e428e888417dbfe7c1e371483153bec2a9f58
Brian Li <61071032+bli31@users.noreply.github.com>
2021-12-13 12:08:42 -0500

    added qcc

--- ALTester.java
+++ ALTester.java
@@ -9,7 +9,8 @@ DISCO
 - Have to import java.util.ArrayList and limit the ArrayList object to only use integers.
 - ArrayList has its own compareTo() method. 
 QCC
-- iou
+- How could you modify this program to work with Objects other than Integer?
+- How would compareTo work with Strings? Other objects?
 */
 
 import java.util.ArrayList;
